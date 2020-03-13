6b3181556ed61731e55be416d81b2e46338a1c3e
Sd2142 <46973830+Sd2142@users.noreply.github.com>
2020-03-13 23:33:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import paramiko
 import socket
 
-commandlist = list['exit', 'lsusb', 'dmidecode -t1', 'sudo su']
-
 def login():
     Hostnamestr = str(input('Enter hostname\n'))
     host = socket.gethostbyname(Hostnamestr)
@@ -10,10 +8,20 @@ def login():
     paramiko_ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         paramiko_ssh_client.connect(hostname=host, username='Login', password='PWD', port=22, timeout=4)
-    except:
+    try:
         paramiko_ssh_client.connect(hostname=host, username='login2', password='PWD2', port=22, timeout=4)
+        print(Style.RESET_ALL)
+    except socket.timeout:
+        print('socket.timeout')
+    except socket.gaierror:
+        print('socket.gaierror')
+    except paramiko.ssh_exception.NoValidConnectionsError:
+        print('paramiko.ssh_exception.NoValidConnectionsError')
+    except TimeoutError:
+        print('TimeoutError')
 
 def runcommand():
+    commandlist = list['exit', 'lsusb', 'dmidecode -t1', 'sudo su']
     EnterCommand = int(input('Exit 0 press 0\n', 'randomcommand1 press 1\n', 'randomcommand2 press 2\n', 'randomcommand3 press 3\n','randomcommand4 press 4\n', ))
     SentCommand = str(commandlist[EnterCommand])
     if EnterCommand == 0:
